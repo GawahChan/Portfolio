d185575f1454741bdaa0975506cb003c9c8d5997
Gawah Chan <gawahchan@gmail.com>
2019-05-03 15:04:45 +0100

    installed router as well as created icons for Nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import {BrowserRouter} from 'react-router-dom'; 
 import Header from './Components/Header/Header';
 import About from './Components/About/About';
 import './App.css';
+
 
 function App() {
   return (
--- src/Components/Header/Navigation/NavigationBar.js
+++ src/Components/Header/Navigation/NavigationBar.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import './NavigationBar.css'
+import './NavigationBar.css';
 
 function NavigationBar() {
     return (
         <div className="NavigationBar">
             <div className="NavigationIcons">
+                <a href="https://github.com/GawahChan" target="_blank" rel="noopener noreferrer">
+                    <i class="fab fa-github"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/gawahchan/" target="_blank" rel="noopener noreferrer">
+                    <i class="fab fa-linkedin"></i>
+                </a>
+                    <i class="far fa-envelope"></i>     
             </div>
-            <div className="NavigationMenu">
-                <h3>Home</h3>
-                <h3>About</h3>
-                <h3>Skills</h3>
-                <h3>Projects</h3>
+                <div className="NavigationMenu">
+                    <h3>Home</h3>
+                    <h3>About</h3>
+                    <h3>Skills</h3>
+                    <h3>Projects</h3>
+                </div>
             </div>
-        </div>
-    );
-}
-
+            );
+        }
+        
 export default NavigationBar;
