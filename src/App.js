69cba5d4e7c6eb5f5c34d01f14639f95c4acc441
Gawah Chan <gawahchan@gmail.com>
2019-05-03 13:29:01 +0100

    Refactor Code as well as changed color of text

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Components/Header/Header';
 import About from './Components/About/About';
+import './App.css';
 
 function App() {
   return (
--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -10,8 +10,9 @@ function About() {
             <p>Hi, my name is Gawah. I'm a Full-Stack developer based in London, currently looking for a Junior Developer Position.
                 I recently graduated from The University of York with a BSc Economics degree and am looking to break into the tech industry.
                 <br /><br /><br />
-                I'm currently a student at Kodiri BootCamp and have experience working with a number of technologies including: React, JavaScript,
-                HTML and CSS.
+                I'm currently a student at Kodiri BootCamp and have experience working with a number of technologies including: 
+                <br /><br />
+                React, JavaScript, HTML and CSS.
                 <br /><br /><br />
                 If you're looking for an ambitious and enthusiastic Junior Full-Stack Developer, please get in touch!
             </p>
