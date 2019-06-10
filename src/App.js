cc45a0adf1ba37a1713f466d8343e435a66ada88
Gawah Chan <gawahchan@gmail.com>
2019-06-11 00:26:37 +0100

    Created Overlay for Porjects section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import About from './Components/About/About';
 import Projects from './Components/Projects/Projects';
@@ -9,14 +8,12 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
       <div className="App">
         <Header />
         <About />
         <Projects />
         {/* <Footer /> */}
       </div>
-    </BrowserRouter>
   );
 }
 
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -9,14 +9,24 @@ function Projects() {
             <h1>My Projects</h1>
             <div className="Projects-list">
                 <div className="Projects-item">
-                    <img src={Kodflix} alt="Kodflix Project"/>
-                    <h2>Kodflix</h2>
+                    <a href="https://kodflix-gawah.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                        <img src={Kodflix} alt="Kodflix Project" />
+                        <div className="Overlay">
+                            <h2>Kodflix</h2>
+                            <p>A netflix inspired web app</p>
+                        </div>
+                    </a>
                 </div>
                 <div className="Projects-item">
-                    <img src={AlphaProps} alt="Alpha-Props Project"/>
-                    <h2>Alpha-props</h2>
+                    <a href="https://alpha-properties-app.appspot.com/" target="_blank" rel="noopener noreferrer">
+                        <img src={AlphaProps} alt="Alpha-Props Project" />
+                        <div className="Overlay">
+                            <h2>Alpha-props</h2>
+                            <p>A mock real estate agency website based on high end properties in London </p>
+                        </div>
+                    </a>
                 </div>
-            </div>  
+            </div>
         </div>
     );
 }
