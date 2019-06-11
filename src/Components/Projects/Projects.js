30a2d528f95fad8139e0e96f12a716823572e785
Gawah Chan <gawahchan@gmail.com>
2019-06-11 12:50:13 +0100

    changed CSS layout for project box

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -9,40 +9,40 @@ function Projects() {
                 <div className="Projects-item kodflix">
                     <a href="https://kodflix-gawah.herokuapp.com/" target="_blank" rel="noopener noreferrer">
                         <div className="Overlay">
-                            <h1>Kodflix</h1>
-                            {/* <p>A netflix inspired web app</p> */}
-                            <div className="Projects-languages">
+                            <h2>Kodflix</h2>
+                            <p>A netflix inspired web app</p>
+                            {/* <div className="Projects-languages">
                                 <i class="fab fa-react fa-2x" />
                                 <i class="fab fa-node fa-2x" />
                                 <i class="fab fa-js-square fa-2x" />
                                 <i class="fab fa-html5 fa-2x" />
                                 <i class="fab fa-css3 fa-2x" />
                                 <i class="fab fa-git fa-2x" />
-                            </div>
+                            </div> */}
                         </div>
                     </a>
                 </div>
                 <div className="Projects-item alpha-props">
                     <a href="https://alpha-properties-app.appspot.com/" target="_blank" rel="noopener noreferrer">
                         <div className="Overlay">
-                            <h1>Alpha-Properties</h1>
-                            {/* <p>A mock real-estate agency website based on properties in London </p> */}
-                            <div className="Projects-languages">
+                            <h2>Alpha-Properties</h2>
+                            <p>A mock real-estate agency website based on properties in London </p>
+                            {/* <div className="Projects-languages">
                                 <i class="fab fa-react fa-2x" />
                                 <i class="fab fa-node fa-2x" />
                                 <i class="fab fa-js-square fa-2x" />
                                 <i class="fab fa-html5 fa-2x" />
                                 <i class="fab fa-css3 fa-2x" />
                                 <i class="fab fa-git fa-2x" />
-                            </div>
+                            </div> */}
                         </div>
                     </a>
                 </div>
                 <div className="Projects-item other-projects">
                     <a href="https://github.com/GawahChan" target="_blank" rel="noopener noreferrer">
                         <div className="Overlay">
-                            <h1>Other Projects</h1>
-                            <i class="fab fa-github fa-2x" />
+                            <h2>Other Projects</h2>
+                            <i class="fab fa-github fa-3x" />
                         </div>
                     </a>
                 </div>
