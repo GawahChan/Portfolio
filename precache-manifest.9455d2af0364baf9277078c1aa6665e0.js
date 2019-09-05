2c746f3782e942be9441865d33759fcda79ef26e
Gawah Chan <gawahchan@gmail.com>
2019-09-05 03:03:32 +0100

    Updates

---
+++ precache-manifest.9455d2af0364baf9277078c1aa6665e0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "030dcd56d2d6ea257d4c0a6cb21cf2be",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2ab2aa16283b880cb9df",
+    "url": "/portfolio/static/css/main.f0954365.chunk.css"
+  },
+  {
+    "revision": "df19cae414ea3e96eb6d",
+    "url": "/portfolio/static/js/2.1448908d.chunk.js"
+  },
+  {
+    "revision": "2ab2aa16283b880cb9df",
+    "url": "/portfolio/static/js/main.b7d6b5a5.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "8443551bf8a771e2863b145bfd9dac08",
+    "url": "/portfolio/static/media/Alpha-props.8443551b.JPG"
+  },
+  {
+    "revision": "ef83d422fb5d17a3b5abed76827ebc75",
+    "url": "/portfolio/static/media/Kodflix.ef83d422.JPG"
+  },
+  {
+    "revision": "45a4eb8906544d28e2a626a44c56dfdb",
+    "url": "/portfolio/static/media/Kodiri profile pic.45a4eb89.PNG"
+  },
+  {
+    "revision": "cc2fe4dddda4f833fef7f67923c00ccb",
+    "url": "/portfolio/static/media/PortugalPhoto.cc2fe4dd.JPG"
+  }
+]);
