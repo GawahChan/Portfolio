712dd34e7f0a378c49f36a14d8d2270b3b1664df
Gawah Chan <gawahchan@gmail.com>
2019-09-12 20:34:32 +0100

    Added more projects and restyled portfolio

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ProjectList from './ProjectsList';
 import './Projects.css';
 
 function Projects() {
@@ -6,30 +7,24 @@ function Projects() {
         <div className="Projects-container" id="Projects">
             <h1>My Projects</h1>
             <div className="Projects-list">
-                <div className="Projects-item kodflix">
-                    <a href="https://kodflix.appspot.com/" target="_blank" rel="noopener noreferrer">
-                        <div className="Overlay">
-                            <h2>Kodflix</h2>
-                            <p>A netflix inspired web app</p>
-                        </div>
-                    </a>
-                </div>
-                <div className="Projects-item alpha-props">
-                    <a href="https://alpha-properties-app.appspot.com/" target="_blank" rel="noopener noreferrer">
-                        <div className="Overlay">
-                            <h2>Alpha-Properties</h2>
-                            <p>A mock real-estate agency website based on properties in London </p>
-                        </div>
-                    </a>
-                </div>
-                <div className="Projects-item other-projects">
-                    <a href="https://github.com/GawahChan" target="_blank" rel="noopener noreferrer">
-                        <div className="Overlay">
-                            <h2>Check out my GitHub!</h2>
-                            <i class="fab fa-github fa-3x" />
-                        </div>
-                    </a>
-                </div>
+                {
+                    ProjectList.map(project => {
+                        return (
+                            <div className={`Projects-item ${project.className}`}>
+                                <a href={project.link} target="_blank" rel="noopener noreferrer">
+                                    <div className="Overlay">
+                                        <h2>{project.name}</h2>
+                                        <p>{project.description}</p>
+                                        <a href={project.gitHub} target="_blank" rel="noopener noreferrer">
+                                            <i class="fab fa-github fa-3x" />
+                                        </a>
+                                        
+                                    </div>
+                                </a>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
     );
---
+++ src/Components/Projects/ProjectsList.js
@@ -0,0 +1,28 @@
+const ProjectsList = [
+    {
+        className: 'Kodflix', name: 'Kodflix', 
+        link:'https://kodflix.appspot.com/', gitHub:'https://github.com/GawahChan/kodflix',
+        description:'A netflix inspired web app'
+    },
+    {
+        className: 'Alpha-Properties', name: 'Alpha-Properties',
+        link:'https://alpha-properties-app.appspot.com/', gitHub:'https://github.com/kodiri/alpha-props',
+        description:'A mock real-estate agency website based on properties in London'
+    },
+    {
+        className: 'Gift-Giver', name: 'Gift Giver', 
+        link:'https://gawahchan.github.io/Gift-giver/', gitHub:'https://github.com/GawahChan/Gift-giver',
+        description:'A revolutionizing way of keeping track of your gift list'
+    },
+    {   
+        className: 'Svelte-Scoreboard', name: 'Svelte Scoreboard',
+        link:'https://public.gawahchan.now.sh/', gitHub:'https://github.com/GawahChan/Svelte-Scoreboard',
+        description:'Experimenting with Svelte to keep track of player scores'
+    },
+    {
+        className: 'Other-Projects', name: 'Other Projects', link:'https://github.com/GawahChan',
+        description:'Check out my GitHub here!',
+    }
+];
+
+export default ProjectsList;
