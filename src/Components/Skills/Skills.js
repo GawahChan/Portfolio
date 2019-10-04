a9aea4a7da203491abfca83defbab49839b98aa4
Gawah Chan <gawahchan@gmail.com>
2019-10-04 17:34:01 +0100

    Refactored Skills Component

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,20 +1,19 @@
 import React from 'react';
-
-import './Skills.css'
+import {SkillsContainer, SkillsTitle, SkillsList} from './style';
 
 function Skills() {
     return (
-        <div className="skills-container" id="Skills">
-            <h1>My Skills</h1>
-            <div className="skills-languages">
+        <SkillsContainer id="Skills">
+            <SkillsTitle>My Skills</SkillsTitle>
+            <SkillsList>
                 <i class="fab fa-react fa-3x" />
                 <i class="fab fa-node fa-3x" />
                 <i class="fab fa-js-square fa-3x" />
                 <i class="fab fa-html5 fa-3x" />
                 <i class="fab fa-css3 fa-3x" />
                 <i class="fab fa-git fa-3x" />
-            </div>
-        </div>
+            </SkillsList>
+        </SkillsContainer>
     );
 }
 
--- src/Components/Skills/style.js
+++ src/Components/Skills/style.js
@@ -1,19 +1,20 @@
-.skills-container {
+import styled from 'styled-components';
+
+export const SkillsContainer = styled.div`
     text-align: center;
-    padding: 3rem 1rem 2rem 1rem;
+    padding: 1rem 1rem 1rem 1rem;
     background-color: rgba(223, 177, 51, 0.6);
     color: white;
-}
+`
 
-.skills-container h1 {
+export const SkillsTitle = styled.h1`
     font-size: 200%;
     font-weight: 500;
-    padding: 0.5rem;
-}
+`
 
-.skills-languages {
+export const SkillsList = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
     grid-gap: 3rem;
     padding: 1rem
-}
+`
