ba914e27dbff6917c146fbbaf05279c101fadd35
Gawah Chan <gawahchan@gmail.com>
2019-10-04 19:17:23 +0100

    Refactored Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './Footer.css';
+import { FooterContainer } from './style';
 
-export default function Footer () {
-    return(
-        <div className="Section-Footer">
-            <h6>Copyright: Made by Gawah Chan</h6>
-        </div>
+
+export default function Footer() {
+    return (
+        <FooterContainer>
+            Copyright: Made by Gawah Chan
+        </FooterContainer>
     );
 }
---
+++ src/Components/Footer/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const FooterContainer = styled.h6`
+    margin: 0;
+    width: 100%;
+
+    background-color:  rgb(50, 50, 50);
+    color: white;
+    
+    font-weight: 200;
+    font-size: 1rem;
+    text-align: center;
+    padding: 0.5rem;
+`
